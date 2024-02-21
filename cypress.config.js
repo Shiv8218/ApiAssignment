c5c464b97ea6f5f0f1a1734b16e55e799e41d097
Shiv8218 <shiv8218.pratap@gmail.com>
2024-02-21 12:19:20 +0530

    Modify config file for video

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 module.exports = {
+  video: true,
   reporter: 'cypress-mochawesome-reporter',  // for_report
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      this.screenshotOnRunFailure=true;
       require('cypress-mochawesome-reporter/plugin')(on);   // for_report
 
     },
